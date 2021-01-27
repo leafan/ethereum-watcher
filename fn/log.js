dc6a0792d493eeb88cfb9b74b20d693cd9634fb7
dynamicdollar <drdollars@protonmail.com>
2021-01-27 22:32:39 +0800

    first commit

--- fn/log.js
+++ fn/log.js
@@ -1,14 +1,9 @@
-const BN = require("bn.js");
 const {getAddress,toAddress} = require('../utils/does_it_exist')
 const Abi = require('../abi/erc20.json');
 module.exports = function () {
 
     web3.eth.subscribe('logs', {
-        // address: '0x7302509251F3bdA6428365bAe08206D4C3D3173C',
-        topics: [
-            '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
-             // '0x0000000000000000000000007302509251F3bdA6428365bAe08206D4C3D3173C',
-        ],
+        topics: ['0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',],
     }, function (error, result) {
     })
         .on("connected", function (subscriptionId) {
@@ -43,9 +38,7 @@ const sendFn = (isForm, entry, data) => {
     last = data.transactionHash
     transactionData[last] = [...(transactionData[last] || []), data]
     timeout = setTimeout(async () => {
-
         let _contract =  new web3.eth.Contract(Abi.abi,  data.address);
-
         let _name = await _contract.methods.symbol().call();
         let _decimals = await _contract.methods.decimals().call();
         let _textTmp = '';
@@ -55,7 +48,6 @@ const sendFn = (isForm, entry, data) => {
             \n amount: ${item.data/Math.pow(10,_decimals)} ${_name} \n------------------------------------------
             `;
         })
-
         entry.chatID.forEach((chatID) => {
             bot.sendMessage(chatID, `address: ${entry.addressNote} ${isForm ? 'incoming funds!' : 'flying out funds!'}
             ${_textTmp}
@@ -63,11 +55,7 @@ const sendFn = (isForm, entry, data) => {
             `);
         });
         delete transactionData[data.transactionHash];
-    }, 100);
-    // console.log(data);
-    // entry.chatID.forEach((chatID)=>{
-    //     bot.sendMessage(chatID, `address: ${entry.addressNote} ${isForm?'incoming funds!':'flying out funds!'}
-    //     \n${data.data} ETH arrived to the address "https://etherscan.io/tx/${data.transactionHash}" since I've last checked.`);
-    // })
+    }, 1000);
+
 }
 
